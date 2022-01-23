dd63840dbad3c9bea2149587256f4c4e44b7883c
Meggin <megginsimon@gmail.com>
2022-01-23 03:48:42 +0000

    checked for palm

--- api_calls.py
+++ api_calls.py
@@ -1,19 +1,25 @@
 import requests
-
+import re
 
 payload = {
-            'query': 'Frosted Flakes',
+            'query': 'Nutella',
             'dataType': 'Branded',
-            'brandOwner': 'Kellogg',
-            'pageSize': '15',
+            'brandOwner': '',
+            'pageSize': '2',
             'api_key': 'fJ2wh3xW6pxbmvirGjlwGhs2gwTaXedDlqxrXofR'
             }
 
 search = requests.get('https://api.nal.usda.gov/fdc/v1/foods/search', params = payload)
 result = search.json()
 
-palm_aliases = 'PALM'
-rspo = ""
+palm_aliases = ["Palm Oil", "Palm Kernel", "Palm Kernel Oil", "Palm Fruit Oil", "Palmate, Palmitate", "Palm olein", "Glyceryl Stearate", 
+                "Stearic Acid", "Elaeis Guineensis", "Palmitic Acid", "Palm Stearine", "Palmitoyl Oxostearamide", 
+                "Palmitoyl Tetrapeptide", "Sodium Laureth Sulfate", "Sodium Lauryl Sulfate", "Sodium Kernelate",
+                "Sodium Palm Kernelate", "Sodium Lauryl Lactylate", "Sulphate", "Hydrated Palm Glycerides", "Etyl Palmitate", 
+                "Octyl Palmitate", "Palmityl Alcohol", "palm"]
+
+p = re.compile(r'([^,]*PALM[^,]*),')
+
 
 foods = result['foods']
 
@@ -24,10 +30,18 @@ for i in range(len(foods)):
     brand = result['foods'][i].get('brandOwner')
     ingredients_string = result['foods'][i].get('ingredients')
     ingredients = ingredients_string.split(", ")
-    if palm_aliases in ingredients:
+    contains_palm = False
+    palm_ingredients = []
+    for palm_alias in palm_aliases:
+        if palm_alias.upper() in ingredients:
+            contains_palm = True
+            palm_ingredients.append(palm_alias.upper())
+    palm_names = p.findall(ingredients_string)
+    if palm_names != []:
         contains_palm = True
-    else:
-        contains_palm = False
+        for palm_name in palm_names:
+            if palm_name not in palm_ingredients:
+                palm_ingredients.append(palm_name)
 
-    print(name, descriptor, fdc_id, brand, contains_palm)
+    print(name, descriptor, fdc_id, brand, contains_palm, ingredients, palm_ingredients)
 
